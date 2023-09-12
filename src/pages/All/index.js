ccdac0851ea9d793b4eca0809b92e7399dd3123d
Juan Pasten Castillo <juanpastencastillo@gmail.com>
2023-09-12 01:09:40 -0300

    Fix - To push to vercel

--- src/pages/All/index.js
+++ src/pages/All/index.js
@@ -1,6 +1,15 @@
 import Link from "next/link"
 import AllContentWrapper from "./styles/AllContentWrapper.js"
 
+/* 
+
+[ts] Already included file name 
+'f:/JuanEnEfe/Informática/Practicando/blog/src/pages/All/styles/AllContentWrapper.js' differs from file name 'f:/JuanEnEfe/Informática/Practicando/blog/src/pages/all/styles/AllContentWrapper.js' only in casing.
+  The file is in the program because:
+    Imported via "./styles/AllContentWrapper.js" from file 'f:/JuanEnEfe/Informática/Practicando/blog/src/pages/All/index.js'
+    Root file specified for compilation
+*/
+
 const AllContent = () => {
 
   return (
@@ -9,16 +18,16 @@ const AllContent = () => {
 
       <ul>
         <li>
-          <Link href="/all/philosophical-reflection">Philosophical Reflection</Link>
+          <Link href="/All/philosophical-reflection">Philosophical Reflection</Link>
         </li>
         <li>
-          <Link href="/all/leadership">How is Leadership According to Research?</Link>
+          <Link href="/All/leadership">How is Leadership According to Research?</Link>
         </li>
         <li>
-          <Link href="/all/founders-interviews">Founders Interviews</Link>
+          <Link href="/All/founders-interviews">Founders Interviews</Link>
         </li>
         <li>
-          <Link href="/all/stress-management">Stress Management</Link>
+          <Link href="/All/stress-management">Stress Management</Link>
         </li>
       </ul>
 
